87ace4b0a0dbe1354f9cc3aa6e81dee1a7360eba
bytecascade11 <okesunday503@gmail.com>
2026-01-18 07:21:34 +0100

    Update astro.config.ts

--- astro.config.ts
+++ astro.config.ts
@@ -37,12 +37,4 @@ export default defineConfig({
       exclude: ["@resvg/resvg-js"],
     },
   },
-
-  experimental: {
-    fonts: {
-      providers: {
-        google: ["Source Code Pro"],
-      },
-    },
-  },
 });
