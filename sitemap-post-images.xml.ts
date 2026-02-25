8348657cc104130cfaf830df731d10e35a51b55d
bytecascade11 <okesunday503@gmail.com>
2026-02-25 02:15:02 +0100

    Update sitemap-post-images.xml.ts

--- sitemap-post-images.xml.ts
+++ sitemap-post-images.xml.ts
@@ -1,29 +1,35 @@
-import { getCollection } from "astro:content";
 import { SITE } from "@/config";
+import { collection } from "astro:content";
 
 export async function get() {
-  const posts = await getCollection("blog");
+  const posts = await collection("blog");
+
+  const items = posts
+    .filter(post => !post.data.draft && post.data.coverImage)
+    .map(post => {
+      const slug = post.id.replace(/^blog\//, "");
+      const postUrl = `${SITE.website}posts/${slug}/`;
+      const lastmod = post.data.modDatetime
+        ? post.data.modDatetime.toISOString()
+        : post.data.pubDatetime.toISOString();
+      const imageUrl = post.data.coverImage.startsWith("http")
+        ? post.data.coverImage
+        : `${SITE.website}${post.data.coverImage.replace(/^\/+/, "")}`;
+
+      return `
+  <url>
+    <loc>${postUrl}</loc>
+    <lastmod>${lastmod}</lastmod>
+    <image:image>
+      <image:loc>${imageUrl}</image:loc>
+    </image:image>
+  </url>`;
+    }).join("\n");
 
   const xml = `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
         xmlns:image="http://www.google.com/schemas/sitemap-image/1.1">
-${posts
-  .map((post) => {
-    const slug = post.id.replace(/^blog\//, "");
-    const postUrl = `${SITE.website}posts/${slug}/`;
-    const coverImage = post.data.coverImage
-      ? new URL(post.data.coverImage, SITE.website).toString()
-      : undefined;
-    if (!coverImage) return ""; // Skip posts without images
-    return `  <url>
-    <loc>${postUrl}</loc>
-    <image:image>
-      <image:loc>${coverImage}</image:loc>
-    </image:image>
-  </url>`;
-  })
-  .filter(Boolean)
-  .join("\n")}
+${items}
 </urlset>`;
 
   return new Response(xml, {
