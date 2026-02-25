9661ce28f70cbcd39afec2aedd1216a08eade68d
bytecascade11 <okesunday503@gmail.com>
2026-02-25 02:28:50 +0100

    Update sitemap-posts.xml.ts

--- sitemap-posts.xml.ts
+++ sitemap-posts.xml.ts
@@ -1,25 +1,28 @@
-import { getCollection, CollectionEntry } from "astro:content";
+// src/pages/sitemap-posts.xml.ts
+import { getCollection } from "astro:content";
+import type { CollectionEntry } from "astro:content";
 
-export async function get() {
+export async function GET() {
+  // Fetch all posts from the "posts" collection
   const posts: CollectionEntry<"posts">[] = await getCollection("posts");
 
-  const sitemap = posts
-    .filter((post) => !post.data.draft)
-    .map((post) => {
-      return `
-        <url>
-          <loc>https://yourdomain.com/${post.slug}/</loc>
-          <lastmod>${post.data.updatedAt ?? post.data.date}</lastmod>
-        </url>
-      `;
-    })
-    .join("");
+  const filteredPosts = posts.filter((post) => !post.data.draft);
 
-  return new Response(
-    `<?xml version="1.0" encoding="UTF-8"?>
-     <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-       ${sitemap}
-     </urlset>`,
-    { headers: { "Content-Type": "application/xml" } }
-  );
+  const xml = `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+  ${filteredPosts
+    .map(
+      (post) => `<url>
+    <loc>https://yourdomain.com/${post.slug}/</loc>
+    <lastmod>${(post.data.updatedAt ?? post.data.date).toISOString()}</lastmod>
+  </url>`
+    )
+    .join("\n")}
+</urlset>`;
+
+  return new Response(xml, {
+    headers: {
+      "Content-Type": "application/xml",
+    },
+  });
 }
