70cfd7dd7f1b5bd7e534333577c9ef48ecb9b65d
bytecascade11 <okesunday503@gmail.com>
2026-02-25 02:27:39 +0100

    Update sitemap-post-images.xml.ts

--- sitemap-post-images.xml.ts
+++ sitemap-post-images.xml.ts
@@ -1,30 +1,32 @@
-import { getCollection, CollectionEntry } from "astro:content";
+// src/pages/sitemap-post-images.xml.ts
+import { getCollection } from "astro:content";
+import type { CollectionEntry } from "astro:content";
 
-export async function get() {
+export async function GET() {
   const posts: CollectionEntry<"posts">[] = await getCollection("posts");
 
-  const sitemap = posts
-    .filter((post) => !post.data.draft && post.data.coverImage)
-    .map((post) => {
-      return `
-        <url>
-          <loc>https://yourdomain.com/${post.slug}/</loc>
-          <image:image>
-            <image:loc>${post.data.coverImage}</image:loc>
-            <image:caption>${post.data.title}</image:caption>
-          </image:image>
-          <lastmod>${post.data.updatedAt ?? post.data.date}</lastmod>
-        </url>
-      `;
-    })
-    .join("");
+  const filteredPosts = posts.filter((post) => !post.data.draft);
 
-  return new Response(
-    `<?xml version="1.0" encoding="UTF-8"?>
-     <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
-             xmlns:image="http://www.google.com/schemas/sitemap-image/1.1">
-       ${sitemap}
-     </urlset>`,
-    { headers: { "Content-Type": "application/xml" } }
-  );
+  const xml = `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
+        xmlns:image="http://www.google.com/schemas/sitemap-image/1.1">
+  ${filteredPosts
+    .map((post) => {
+      if (!post.data.coverImage) return "";
+      return `<url>
+    <loc>https://yourdomain.com/${post.slug}/</loc>
+    <image:image>
+      <image:loc>${post.data.coverImage}</image:loc>
+      <image:caption>${post.data.coverImageAlt ?? ""}</image:caption>
+    </image:image>
+  </url>`;
+    })
+    .join("\n")}
+</urlset>`;
+
+  return new Response(xml, {
+    headers: {
+      "Content-Type": "application/xml",
+    },
+  });
 }
