5f54e02a44b15bc3532d1fa80018ecb6b0de16d5
bytecascade11 <okesunday503@gmail.com>
2026-02-25 02:12:03 +0100

    Update sitemap-index.xml.ts

--- sitemap-index.xml.ts
+++ sitemap-index.xml.ts
@@ -1,16 +1,22 @@
 import { SITE } from "@/config";
 
-export function GET() {
-  return new Response(
-    `<?xml version="1.0" encoding="UTF-8"?>
-    <sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-      <sitemap>
-        <loc>${SITE.website}sitemap-posts.xml</loc>
-      </sitemap>
-      <sitemap>
-        <loc>${SITE.website}sitemap-post-images.xml</loc>
-      </sitemap>
-    </sitemapindex>`,
-    { headers: { "Content-Type": "application/xml" } }
-  );
+const now = new Date().toISOString();
+
+export async function get() {
+  const xml = `<?xml version="1.0" encoding="UTF-8"?>
+<sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+  <sitemap>
+    <loc>${SITE.website}sitemap-posts.xml</loc>
+    <lastmod>${now}</lastmod>
+  </sitemap>
+  <sitemap>
+    <loc>${SITE.website}sitemap-post-images.xml</loc>
+    <lastmod>${now}</lastmod>
+  </sitemap>
+</sitemapindex>`;
+
+  return new Response(xml, {
+    status: 200,
+    headers: { "Content-Type": "application/xml" },
+  });
 }
