23cb02b3187152760b9557ba329d1097c27d2c83
bytecascade11 <okesunday503@gmail.com>
2026-02-25 02:04:49 +0100

    Update sitemap-post-images.xml.ts

--- sitemap-post-images.xml.ts
+++ sitemap-post-images.xml.ts
@@ -1,28 +1,33 @@
 import { getCollection } from "astro:content";
 import { SITE } from "@/config";
 
-export async function GET() {
+export async function get() {
   const posts = await getCollection("blog");
 
-  const urls = posts.map(post => {
-    const postUrl = new URL(`/posts/${post.slug}/`, SITE.website).toString();
-    const imageUrl = post.data.coverImage
+  const xml = `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
+        xmlns:image="http://www.google.com/schemas/sitemap-image/1.1">
+${posts
+  .map((post) => {
+    const slug = post.id.replace(/^blog\//, "");
+    const postUrl = `${SITE.website}posts/${slug}/`;
+    const coverImage = post.data.coverImage
       ? new URL(post.data.coverImage, SITE.website).toString()
-      : "";
+      : undefined;
+    if (!coverImage) return ""; // Skip posts without images
+    return `  <url>
+    <loc>${postUrl}</loc>
+    <image:image>
+      <image:loc>${coverImage}</image:loc>
+    </image:image>
+  </url>`;
+  })
+  .filter(Boolean)
+  .join("\n")}
+</urlset>`;
 
-    return `
-      <url>
-        <loc>${postUrl}</loc>
-        ${imageUrl ? `<image:image><image:loc>${imageUrl}</image:loc></image:image>` : ""}
-      </url>`;
+  return new Response(xml, {
+    status: 200,
+    headers: { "Content-Type": "application/xml" },
   });
-
-  return new Response(
-    `<?xml version="1.0" encoding="UTF-8"?>
-    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
-            xmlns:image="http://www.google.com/schemas/sitemap-image/1.1">
-      ${urls.join("")}
-    </urlset>`,
-    { headers: { "Content-Type": "application/xml" } }
-  );
 }
