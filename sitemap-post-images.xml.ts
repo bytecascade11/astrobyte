a3f723304d1945221af2f7cae3f3e59dab477a02
bytecascade11 <okesunday503@gmail.com>
2026-02-25 02:33:15 +0100

    Update sitemap-post-images.xml.ts

--- sitemap-post-images.xml.ts
+++ sitemap-post-images.xml.ts
@@ -1,13 +1,8 @@
-// src/pages/sitemap-post-images.xml.ts
-import { getCollection } from "astro:content";
-import type { CollectionEntry } from "astro:content";
+const posts: CollectionEntry<"blog">[] = await getCollection("blog");
 
-export async function GET() {
-  const posts: CollectionEntry<"posts">[] = await getCollection("posts");
+const filteredPosts = posts.filter((post) => !post.data.draft);
 
-  const filteredPosts = posts.filter((post) => !post.data.draft);
-
-  const xml = `<?xml version="1.0" encoding="UTF-8"?>
+const xml = `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
         xmlns:image="http://www.google.com/schemas/sitemap-image/1.1">
   ${filteredPosts
@@ -23,10 +18,3 @@ export async function GET() {
     })
     .join("\n")}
 </urlset>`;
-
-  return new Response(xml, {
-    headers: {
-      "Content-Type": "application/xml",
-    },
-  });
-}
