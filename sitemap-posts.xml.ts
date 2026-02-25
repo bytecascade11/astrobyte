d4cab901b9c6764eb1d45900bfd8d49693b13f77
bytecascade11 <okesunday503@gmail.com>
2026-02-25 02:04:01 +0100

    Update sitemap-posts.xml.ts

--- sitemap-posts.xml.ts
+++ sitemap-posts.xml.ts
@@ -1,19 +1,26 @@
 import { getCollection } from "astro:content";
 import { SITE } from "@/config";
 
-export async function GET() {
+export async function get() {
   const posts = await getCollection("blog");
 
-  const urls = posts.map(post => {
-    const postUrl = new URL(`/posts/${post.slug}/`, SITE.website).toString();
-    return `<url><loc>${postUrl}</loc></url>`;
-  });
+  const xml = `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+${posts
+  .map((post) => {
+    // Compute slug from post.id (removes 'blog/' prefix)
+    const slug = post.id.replace(/^blog\//, "");
+    const url = `${SITE.website}posts/${slug}/`;
+    return `  <url>
+    <loc>${url}</loc>
+    <lastmod>${post.data.modDatetime?.toISOString() || post.data.pubDatetime.toISOString()}</lastmod>
+  </url>`;
+  })
+  .join("\n")}
+</urlset>`;
 
-  return new Response(
-    `<?xml version="1.0" encoding="UTF-8"?>
-    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-      ${urls.join("")}
-    </urlset>`,
-    { headers: { "Content-Type": "application/xml" } }
-  );
+  return new Response(xml, {
+    status: 200,
+    headers: { "Content-Type": "application/xml" },
+  });
 }
