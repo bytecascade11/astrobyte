6b6d3643fa7b0ad7a9c20764d78783f3b0b20965
bytecascade11 <okesunday503@gmail.com>
2026-02-25 02:37:42 +0100

    Update sitemap-posts.xml.ts

--- sitemap-posts.xml.ts
+++ sitemap-posts.xml.ts
@@ -1,26 +1,35 @@
 import { getCollection } from "astro:content";
-import type { CollectionEntry } from "astro:content";
 
 export async function GET() {
-  // Use the correct collection name: "blog"
-  const posts: CollectionEntry<"blog">[] = await getCollection("blog");
+  // Get all posts from the "posts" collection
+  const posts = await getCollection("posts");
 
-  // Only published posts
-  const filteredPosts = posts.filter((post) => !post.data.draft);
+  // Filter out draft posts
+  const publishedPosts = posts.filter((post) => !post.data.draft);
 
-  const xml = `<?xml version="1.0" encoding="UTF-8"?>
-<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-  ${filteredPosts
-    .map(
-      (post) => `<url>
-    <loc>https://yourdomain.com/${post.slug}/</loc>
-    <lastmod>${(post.data.updatedAt ?? post.data.pubDatetime).toISOString()}</lastmod>
-  </url>`
-    )
-    .join("\n")}
+  // Build XML entries
+  const sitemapEntries = publishedPosts
+    .map((post) => {
+      const lastmod = (post.data.updatedAt ?? post.data.date).toISOString();
+      return `
+  <url>
+    <loc>https://www.revibyte.blog/posts/${post.slug}/</loc>
+    <lastmod>${lastmod}</lastmod>
+  </url>`;
+    })
+    .join("");
+
+  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+<urlset
+  xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
+  xmlns:xhtml="http://www.w3.org/1999/xhtml"
+>
+  ${sitemapEntries}
 </urlset>`;
 
-  return new Response(xml, {
-    headers: { "Content-Type": "application/xml" },
+  return new Response(sitemap, {
+    headers: {
+      "Content-Type": "application/xml",
+    },
   });
 }
