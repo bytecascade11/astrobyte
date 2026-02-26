eb8d0150907b4fd1710158df40d90892819f9a42
bytecascade11 <okesunday503@gmail.com>
2026-02-26 14:01:10 +0100

    Update env.d.ts

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,5 +1,12 @@
+typescript
 /// <reference types="astro/client" />
 
 interface Window {
   OneSignalDeferred: Array<(OneSignal: any) => void | Promise<void>>;
+  installApp: () => Promise<void>;
+}
+
+interface BeforeInstallPromptEvent extends Event {
+  prompt(): Promise<void>;
+  userChoice: Promise<{ outcome: 'accepted' | 'dismissed' }>;
 }
