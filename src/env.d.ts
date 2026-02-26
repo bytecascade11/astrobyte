839ce24e4c0f383f5d1646e77f517e66651e7a65
bytecascade11 <okesunday503@gmail.com>
2026-02-26 13:30:33 +0100

    Update env.d.ts

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,11 +1,5 @@
-<script is:inline src="https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.page.js" defer></script>
-<script is:inline>
-  window.OneSignalDeferred = window.OneSignalDeferred || [];
-  window.OneSignalDeferred.push(async function(OneSignal) {
-    await OneSignal.init({
-      appId: "bbd38552-99d1-4cda-96f9-5c0cacfba192",
-      notifyButton: { enable: true }
-    });
-    OneSignal.showNativePrompt();
-  });
-</script>
+/// <reference types="astro/client" />
+
+interface Window {
+  OneSignalDeferred: Array<(OneSignal: any) => void | Promise<void>>;
+}
