9996ca76d17af56721e473bdd8eba8a08046a569
bytecascade11 <okesunday503@gmail.com>
2026-02-26 12:48:34 +0100

    Update env.d.ts

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,14 +1,9 @@
-<!-- OneSignal Push Notifications -->
-    <script src="https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.page.js" defer></script>
-    <script>
-      window.OneSignalDeferred = window.OneSignalDeferred || [];
-      OneSignalDeferred.push(async function(OneSignal) {
-        await OneSignal.init({
-          appId: "bbd38552-99d1-4cda-96f9-5c0cacfba192",
-          notifyButton: { enable: true } // optional bell icon
-        });
-
-        // Automatically show browser prompt
-        OneSignal.showNativePrompt();
-      });
-    </script>
+<script src="https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.page.js" defer></script>  
+<script>  
+  window.OneSignalDeferred = window.OneSignalDeferred || [];  
+  OneSignalDeferred.push(async function(OneSignal) {  
+    await OneSignal.init({  
+      appId: "bbd38552-99d1-4cda-96f9-5c0cacfba192",  
+    });  
+  });  
+</script>
