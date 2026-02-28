d6147baca2fa7cc9a9cda3c66594c28c671f95ab
bytecascade11 <okesunday503@gmail.com>
2026-02-28 21:10:29 +0100

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,23 +1,44 @@
-const CACHE_NAME = "revibyte-v1";
+const CACHE_NAME = "revibyte-v2";
+
+const urlsToCache = [
+  "/",
+  "/offline.html",
+  "/android-chrome-192x192.png",
+  "/android-chrome-512x512.png"
+];
 
 self.addEventListener("install", event => {
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll([
-        "/",
-        "/offline.html",
-        "/icons/android-chrome-192x192.png"
-      ]);
+      return cache.addAll(urlsToCache);
     })
   );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.filter(key => key !== CACHE_NAME)
+            .map(key => caches.delete(key))
+      );
+    })
+  );
+  self.clients.claim();
 });
 
 self.addEventListener("fetch", event => {
   event.respondWith(
-    fetch(event.request).catch(() => {
-      return caches.match(event.request).then(response => {
-        return response || caches.match("/offline.html");
-      });
-    })
+    fetch(event.request)
+      .then(response => {
+        return response;
+      })
+      .catch(() => {
+        return caches.match(event.request)
+          .then(response => {
+            return response || caches.match("/offline.html");
+          });
+      })
   );
 });
